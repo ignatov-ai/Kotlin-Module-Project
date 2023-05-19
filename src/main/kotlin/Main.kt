9e8b8547adc7acb08b5bd9d669ed67af7713d8ca
ignatov-ai <ignatov-ai@yandex.ru>
2023-05-19 11:58:25 +0300

    Начало

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val archiveSelectLevel: MutableList<String> = mutableListOf("1. Выбрать архив","2. Создать архив", "3. Выйти из приложения")
+    val archiveOpenLevel: MutableList<String> = mutableListOf("1. Выбрать заметку","2. Создать заметку", "3. Вернуться к архивам")
+    val noteOpenLevel: MutableList<String> = mutableListOf("1. Посмотреть заметку","2. Вернуться к заметкам")
+
+    val menu = Menu(archiveSelectLevel)
+    menu.showMenuLevel()
+}
+
+class Menu(val menuList: MutableList<Pair<String,()->Unit>>){
+
+    fun showMenuLevel(){
+        println("Выберите пункт меню:")
+        menuList.forEach{println(it)}
+    }
+
+    fun choiseMenuLevel(choise: Int): Boolean{
+        if (choise < 1 || choise > menuList.size){
+            println("Нет такого пункта в меню")
+            return false
+        }
+        val selectedMenu = menuList[choise]
+        selectedMenu.second.invoke()
+        return true
+    }
 }
